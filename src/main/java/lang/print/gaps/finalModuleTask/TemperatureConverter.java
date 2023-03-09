4277fad89bd2d99740e16e8f47fc1cc66d6d4903
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-09 14:04:52 +0600

    Update TemperatureConverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -3,10 +3,10 @@ package lang.print.gaps.finalModuleTask;
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
         float fahrenheit;
-		float firstVar = 9.0f;
-		float secondVar = 5.0f;
-		float plusVar = 32.0f;
-		fahrenheit = ((temperatureCelsius * firstVar / secondVar)+plusVar);
-		System.out.println(fahrenheit);
+	float firstVar = 9.0f;
+	float secondVar = 5.0f;
+	float plusVar = 32.0f;
+	fahrenheit = ((temperatureCelsius * firstVar / secondVar)+plusVar);
+	System.out.println(fahrenheit);
     }
 }
