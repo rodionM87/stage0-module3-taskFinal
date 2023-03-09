7687fc2443b6cf58f5d7054a1b4abb87c06a850f
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-09 14:04:12 +0600

    Update NumbersPrinter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -3,7 +3,7 @@ package lang.print.gaps.finalModuleTask;
 public class NumbersPrinter {
     public void printIsPositive(int number) {
         if (number >= 0) {
-			System.out.println(number);
+	    System.out.println(number);
         }
     }
 }
