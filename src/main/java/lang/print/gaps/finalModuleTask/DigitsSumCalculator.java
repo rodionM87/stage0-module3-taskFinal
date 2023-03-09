2e17f2bf0287dc2b316dd53c95e73ec22b42e2e9
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-09 15:41:28 +0600

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        long nunmber, sum;
+        for (sum=0, number=0, number=number/10) {
+            sum = sum + number % 10;
+        }    
+        System.out.println(sum);
     }
 }
