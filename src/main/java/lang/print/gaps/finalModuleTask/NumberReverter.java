350538b9ff69ea5a22ed3ddefb8220223a216bf0
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-09 14:03:16 +0600

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -3,24 +3,24 @@ package lang.print.gaps.finalModuleTask;
 public class NumberReverter {
     public void revert(int number) {
         Object lengthVar;
-		lengthVar = checkLenghthDigit(number);
-		int lengthInt = (int) lengthVar;
-		int reversed = 0;
-		for (int i = 1; i <= lengthInt; i++) {
-			int tempNumber = number % 10;
-			reversed = reversed * 10 + tempNumber;
-			number /= 10;
-		}
-		System.out.println(reversed);
+	lengthVar = checkLenghthDigit(number);
+	int lengthInt = (int) lengthVar;
+	int reversed = 0;
+	for (int i = 1; i <= lengthInt; i++) {
+	    int tempNumber = number % 10;
+	    reversed = reversed * 10 + tempNumber;
+	    number /= 10;
+	}
+	System.out.println(reversed);
     }
 }
 
     public static Object checkLenghthDigit(int number) {
-		int length = 0;
-		long temp = 1;
-		while (temp <= number) {
-			length++;
-			temp *= 10;
-		}
-		return length;
+	int length = 0;
+	long temp = 1;
+	while (temp <= number) {
+	    length++;
+	    temp *= 10;
 	}
+	return length;
+    }
